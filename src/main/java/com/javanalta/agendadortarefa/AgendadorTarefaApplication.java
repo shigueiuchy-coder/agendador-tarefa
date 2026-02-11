e44d11423398982473f9689d7504914a5caa2ae5
shigueiuchy-coder <shigueiuchy@gmail.com>
2026-02-11 20:39:24 -0300

    commint einicial com esqueleto

---
+++ src/main/java/com/javanalta/agendadortarefa/AgendadorTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.javanalta.agendadortarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanalta/agendadortarefa/AgendadorTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanalta.agendadortarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
