07f24d4fcbc60bdb8cb0985e2e0dcc88d895aa9b
shigueiuchy-coder <shigueiuchy@gmail.com>
2026-02-11 21:33:46 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/javanalta/agendadortarefa/infastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.javanalta.agendadortarefa.infastructure.security;
+
+import com.javanalta.agendadortarefa.business.dto.UsuarioDTO;
+import com.javanalta.agendadortarefa.infastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioClient client;
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build();
+    }
+}
